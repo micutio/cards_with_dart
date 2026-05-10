4309f1f10a59690a23438ce5dc65f1e388b85a35
micutio <wagner.mchl@googlemail.com>
2026-05-10 19:42:34 +0800

    basic multiple choice game

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,31 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  var isGameLost = false;
+  var isGameWon = false;
+
+  print('Cards with Dart');
+  print('Choose a card ([1], [2]): ');
+
+  var choice = stdin.readLineSync();
+  if (choice == null) {
+    print('input error');
+    exit(1);
+  }
+
+  var cardNumber = int.tryParse(choice);
+
+  if (cardNumber == null) {
+    print('this is not a valid number between 1 and 2');
+    exit(1);
+  }
+
+  isGameLost = cardNumber == 1;
+  isGameWon = cardNumber == 2;
+
+  if (isGameLost) print('You lost!');
+
+  if (isGameWon) {
+    print('You won!');
+  }
 }
